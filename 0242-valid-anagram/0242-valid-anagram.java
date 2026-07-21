0357f45615ea8a45a55b91a81ab352298ba0476f
Utkarsh patrikar <137105846+utkarsh232005@users.noreply.github.com>
2026-07-22 00:07:29 +0530

    Time: 4 ms (82.32%), Space: 46.8 MB (5.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,22 +1,36 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         HashMap<Character,Integer> map = new HashMap<>();
+//         for(int i=0;i<s.length();i++){
+//             char ch = s.charAt(i);
+//             map.put(ch,map.getOrDefault(ch,0)+1);
+//         }
+//         for(int i=0;i<t.length();i++){
+//             char ch = t.charAt(i);
+//             if(map.get(ch)!=null){
+//                 if(map.get(ch) == 1){
+//                     map.remove(ch);
+//                 }else{
+//                     map.put(ch,map.get(ch)-1);
+//                 }
+//             }else{
+//                 return false;
+//             }
+//         }
+//         return map.isEmpty();
+//     }
+// }
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        }
-        for(int i=0;i<t.length();i++){
-            char ch = t.charAt(i);
-            if(map.get(ch)!=null){
-                if(map.get(ch) == 1){
-                    map.remove(ch);
-                }else{
-                    map.put(ch,map.get(ch)-1);
-                }
-            }else{
-                return false;
-            }
-        }
-        return map.isEmpty();
+      char[] arrs = s.toCharArray();
+      char[] arrt = t.toCharArray();
+      Arrays.sort(arrs);
+      Arrays.sort(arrt);
+      if(Arrays.equals(arrs,arrt)){
+        return true;
+      }else{
+        return false;
+      }
     }
 }
