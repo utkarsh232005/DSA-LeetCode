a931137f2764dea2c9691970f5f7efddfba89829
Utkarsh patrikar <137105846+utkarsh232005@users.noreply.github.com>
2026-07-25 02:23:53 +0530

    Time: 8 ms (38.69%), Space: 43.7 MB (15.41%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
@@ -1,19 +1,33 @@
-class Solution {
-    public boolean areOccurrencesEqual(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
+// class Solution {
+//     public boolean areOccurrencesEqual(String s) {
+//         HashMap<Character, Integer> map = new HashMap<>();
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch = s.charAt(i);
+//             map.put(ch, map.getOrDefault(ch, 0) + 1);
+//         }
+//         int curr = map.get(s.charAt(0));
+//         for (int i = 1; i < s.length(); i++) {
+//             char ch = s.charAt(i);
+//             if (map.get(ch) != curr) {
+//                 return false;
+//             } else {
+//                 curr = map.get(ch);
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+//short approach HashMap + HashSet 
+
+class Solution{
+    public boolean areOccurrencesEqual(String s){
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i =0;i<s.length();i++){
             char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        int curr = map.get(s.charAt(0));
-        for (int i = 1; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (map.get(ch) != curr) {
-                return false;
-            } else {
-                curr = map.get(ch);
-            }
-        }
-        return true;
+        HashSet<Integer> set = new HashSet<>(map.values());
+        return set.size() == 1;
     }
 }
